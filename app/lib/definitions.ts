4352927c6cf59f055efa5c0fb42490487f40d2aa
trailblazer04 <leofrancis6988@gmail.com>
2025-09-22 20:07:44 +0530

    Completed Mutating Data

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -16,14 +16,22 @@ export type Customer = {
   image_url: string;
 };
 
+// export type Invoice = {
+//   id: string;
+//   customer_id: string;
+//   amount: number;
+//   date: string;
+//   // In TypeScript, this is called a string union type.
+//   // It means that the "status" property can only be one of the two strings: 'pending' or 'paid'.
+//   status: 'pending' | 'paid';
+// };
+
 export type Invoice = {
-  id: string;
+  id: string; // Will be created on the database
   customer_id: string;
-  amount: number;
-  date: string;
-  // In TypeScript, this is called a string union type.
-  // It means that the "status" property can only be one of the two strings: 'pending' or 'paid'.
+  amount: number; // Stored in cents
   status: 'pending' | 'paid';
+  date: string;
 };
 
 export type Revenue = {
